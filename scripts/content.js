45d6c0947fbb6819a38442a7cd2b02a40b633cd4
December G <dgarnetsmith@gmail.com>
2024-06-21 10:03:20 -0500

    Check for not-pressed subtitles button

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,23 @@
-let button = document.querySelector(".ytp-subtitles-button");
+let button = document.querySelector('[aria-pressed=false]');
 let body = document.querySelector("body");
+let config = { childList: true };
 
-button.click();
-console.log('button should have been clicked')
+ let counter = 0;
+
+const callback = (mutationList, observer) => {
+  mutationList.forEach((mutation) => {
+    console.log(button)
+    if (button.classList.contains('ytp-subtitles-button') && counter === 0) {
+      counter++
+      button.click()
+      console.log('in button click', counter)
+      observer.disconnect();
+    } else {
+      observer.disconnect()
+    }
+  });
+};
+
+const observer = new MutationObserver(callback);
+
+observer.observe(body, config);
